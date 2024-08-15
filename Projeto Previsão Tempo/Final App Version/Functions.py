9b95eebc9e34b2298463772f6bcf158791624978
Brian Monzani <monzaniobrian31@gmail.com>
2024-08-15 18:26:28 -0300

    Adicionado Try para evitar que o progama quebre

--- Projeto Previsão Tempo/Final App Version/Functions.py
+++ Projeto Previsão Tempo/Final App Version/Functions.py
@@ -9,13 +9,17 @@ def get_coordinates():
     r = requests.get('http://www.geoplugin.net/json.gp')
 
     if (r.status_code!= 200):
-        print('Não foi possivel obter a localização (get_coordinates)')
+        print('Não foi possivel obter as coordenadas (get_coordinates)')
+        return None
     else:
-        localizacao = (json.loads(r.text))
-        coordenadas = {}
-        coordenadas ['lat'] = localizacao['geoplugin_latitude']
-        coordenadas ['long'] = localizacao['geoplugin_longitude']
-        return coordenadas
+        try:
+            localizacao = (json.loads(r.text))
+            coordenadas = {}
+            coordenadas ['lat'] = localizacao['geoplugin_latitude']
+            coordenadas ['long'] = localizacao['geoplugin_longitude']
+            return coordenadas
+        except:
+            return None
     
 def get_local_code(lat,long):
     locationAPIUrl = (
@@ -26,13 +30,17 @@ def get_local_code(lat,long):
     r = requests.get(locationAPIUrl)
 
     if (r.status_code != 200):
-        print('Não foi possivel obter o codigo do local (get_local_code)')
+        print('Não foi possivel obter a localização (get_local_code)')
+        return None
     else:
-        location_response = json.loads(r.text)
-        info_local = {}
-        info_local ['nome_local']  = location_response['LocalizedName'] + " - " + location_response['AdministrativeArea']['LocalizedName'] + " - " + location_response['Country']['LocalizedName']
-        info_local ['codigo_local'] = location_response['Key']
-        return info_local
+        try:
+            location_response = json.loads(r.text)
+            info_local = {}
+            info_local ['nome_local']  = location_response['LocalizedName'] + " - " + location_response['AdministrativeArea']['LocalizedName'] + " - " + location_response['Country']['LocalizedName']
+            info_local ['codigo_local'] = location_response['Key']
+            return info_local
+        except:
+            return None
 
 def get_present_condition(codigo_local, nome_local):
         current_conditionsAPIUrl =(
@@ -41,11 +49,15 @@ def get_present_condition(codigo_local, nome_local):
         )
         r = requests.get(current_conditionsAPIUrl)
         if (r.status_code != 200):
-            print('Não foi possível obter condição Local (get_present_condition)')
+            print('Não foi possível obter Clima Local (get_present_condition)')
+            return None
         else:
-            current_conditions_response = json.loads(r.text)
-            info_clima = {}
-            info_clima ['text_clima'] = current_conditions_response[0]['WeatherText']
-            info_clima ['temperatura'] = current_conditions_response[0]['Temperature']['Metric']['Value']
-            info_clima ['nome_local'] = nome_local
-            return info_clima
+            try:
+                current_conditions_response = json.loads(r.text)
+                info_clima = {}
+                info_clima ['text_clima'] = current_conditions_response[0]['WeatherText']
+                info_clima ['temperatura'] = current_conditions_response[0]['Temperature']['Metric']['Value']
+                info_clima ['nome_local'] = nome_local
+                return info_clima
+            except:
+                return None
--- Projeto Previsão Tempo/Final App Version/main.py
+++ Projeto Previsão Tempo/Final App Version/main.py
@@ -2,9 +2,11 @@ import requests
 import json
 import Functions
 
-coordenadas = Functions.get_coordinates()
-local = Functions.get_local_code(coordenadas['lat'], coordenadas['long'])
-ClimaAtual = Functions.get_present_condition(local['codigo_local'], local['nome_local'])
-
-print("Clima atual em: " + (ClimaAtual['nome_local']))
-print("Temperatura: ", ClimaAtual['temperatura'])
+try:
+    coordenadas = Functions.get_coordinates()
+    local = Functions.get_local_code(coordenadas['lat'], coordenadas['long'])
+    ClimaAtual = Functions.get_present_condition(local['codigo_local'], local['nome_local'])
+    print("Clima atual em: " + (ClimaAtual['nome_local']))
+    print(f"Temperatura: {ClimaAtual['temperatura']} \xb0C - {ClimaAtual['text_clima']}")
+except:
+    print('Erro ao Processar Solicitação. Entre em contato com Administrador')
